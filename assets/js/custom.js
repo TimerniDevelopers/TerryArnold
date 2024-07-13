0b71f29f1c68e709c6bace0ea42b301718470fb6
RadowanTri <radowan.bhuiyan.tri@gmail.com>
2024-07-13 23:19:58 +0600

    final

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -79,5 +79,23 @@ $(document).ready(function($) {
         }
 	 });
 
+	 $('.partner-list').owlCarousel({
+          loop: true,
+          margin: 10,
+          nav: false,
+          dots: false,
+          responsive: {
+            0: {
+              items: 1
+            },
+            600: {
+              items: 3
+            },
+            1000: {
+              items: 5
+            }
+          }
+        });
+
 
 });
